e6b09ebe8198bc9f38b312681566707574455dc6
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-08-01 12:42:44 -0300

    Add files via upload

---
+++ funcoesnoJavaScript/funcoes.js
@@ -0,0 +1,10 @@
+saldo = 1000
+function dadosacesso(){
+    numcartao = prompt("Digite o numero do cartao: ")
+    senha = prompt("Digite sua senha: ")
+}
+
+function saque(valor){
+    saldo = saldo - valor
+    return saldo
+}
