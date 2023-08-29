b4d2e1142259a36de2ddeaf76b837d5792f1c743
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-08-29 12:35:44 -0300

    Add files via upload

---
+++ Atividade_IMC/imc2.js
@@ -0,0 +1,26 @@
+function calcula_imc(event){
+
+    event.preventDefault(); //Evita o envio do formulário
+
+    var altura = document.imcForm.altura.value;
+    var peso = document.imcForm.peso.value;
+
+    var calculo = peso / (altura * altura);
+
+    var resultado;
+
+    if(calculo<18.5){
+        resultado = "Você está magro com esse indice: " + calculo;
+    }else if(calculo>=18.5 && calculo<24.9){
+        resultado ="Você está normal com esse indice: " + calculo;
+    }else if(calculo>=25 && calculo<29.9){
+        resultado = "Voc~e está com sobre peso com esse indice: " + calculo;
+    }else if(calculo>=30 && calculo<39.9){
+        resultado = "Você está com obesidade com esse indice: " + calculo;
+    }else if(calculo>40){
+        resultado = "Você está com obesidade grave com esse indice: " + calculo;
+    }
+
+    document.getElementById('resul').innerHTML = resultado
+
+}
